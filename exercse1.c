42de28f892100b4c10890ac4d39e4f01adb605b7
eric kyania <erickyania0@gmail.com>
2023-04-26 00:57:30 -0700

    made changes

--- exercse1.c
+++ exercse1.c
@@ -1,55 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <string.h>
 
-#define MAX_COMMAND_LENGTH 1024
+#define PROMPT "#cisfun$ "
 
 /**
- * main: start  execution
+ * main- check for unix
  *
- * unix: unix command interpreter
- * return: 0 on sucess
+ * unix: writting a command in unix
+ * return: 0 if success
  */
-
 
 int main(void)
 {
-    char command[MAX_COMMAND_LENGTH];
+    char command[1024];
     char *args[] = { NULL };
-    pid_t pid;
     int status;
-
     while (1) {
-        // Display prompt and read user input
-        printf("$ ");
-        if (!fgets(command, MAX_COMMAND_LENGTH, stdin)) {
+        // Display prompt
+        printf(PROMPT);
+        // Read user input
+        if (fgets(command, sizeof(command), stdin) == NULL) {
             // Handle end-of-file condition
+            printf("\n");
             break;
         }
-
         // Remove trailing newline character
         command[strcspn(command, "\n")] = '\0';
-
         // Fork process to execute command
-        pid = fork();
+        pid_t pid = fork();
         if (pid < 0) {
             perror("fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
             // Child process: execute command
-            execve(command, args, environ);
-            perror("execve");
+            execve(command, args, NULL);
+            // If execve returns, it means the command could not be executed
+            printf("%s: command not found\n", command);
             exit(EXIT_FAILURE);
         } else {
             // Parent process: wait for child to finish
-            do {
-                if (waitpid(pid, &status, WUNTRACED) == -1) {
-                    perror("waitpid");
-                    exit(EXIT_FAILURE);
-                }
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+            wait(&status);
         }
     }
 
